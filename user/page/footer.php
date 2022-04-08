c83b70a1590ba53af8c03f0f1d9f46794a0a3bd1
A Công <acongkplpe@gmail.com>
2022-04-09 00:06:25 +0700

    upload file footer

---
+++ user/page/footer.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/footerStyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <footer>
+        <div class="c__container">
+            <div class="left box">
+                <div class="upper">
+                    <div class="topic">About me</div>
+                    <p>Ben Quick spent several years exploring the deserts and mountain ranges in the Western US before settling down, becoming a proud father, and earning a BS in natural resource management and an MS in American Studies from Utah State
+                        University in Logan, Utah. </p>
+                </div>
+                <div class="lower">
+                    <div class="topic">Contact me</div>
+                    <div class="phone">
+                        <a href="#"><i class="fas fa-phone-volume"></i>+0079089 6767</a>
+                    </div>
+                    <div class="email">
+                        <a href="#"><i class="fas fa-envelope"></i>abc@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="middle box">
+                <div class="topic">OTHER LINKS</div>
+                <div><a href="#">Online store</a></div>
+                <div><a href="#">Museum</a></div>
+                <div><a href="#">Contact</a></div>
+                <div><a href="#">Story</a></div>
+
+            </div>
+            <div class="right box">
+                <div class="topic">Subscribe us</div>
+                <form action="#">
+                    <input type="text" placeholder="Enter email address">
+                    <input type="submit" name="" value="Send">
+                    <div class="media-icons">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="c__bottom">
+            <p>Photo @<a href="#">Ben Quick</a> All rights reserved
+            </p>
+        </div>
+
+    </footer>
+</body>
+
+</html>
